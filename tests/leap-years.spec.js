4146e8fcad3d40098c3a644e22b402d120eba89a
kelsonbatista <kelsonbatista@yahoo.co.uk>
2021-11-14 00:03:40 -0300

    Requisito 7

--- tests/leap-years.spec.js
+++ tests/leap-years.spec.js
@@ -1,5 +1,5 @@
 // Importação do objeto isLeapYear, não precisa se preocupar com isso por agora.
-const isLeapYear = require('../src/objisLeapYear');
+const isLeapYear = require('../src/leap-years.js');
 
 /*
   Essa função simplesmente determina se um ano é bissexto ou não e retorna true ou false.
@@ -18,3 +18,15 @@ const isLeapYear = require('../src/objisLeapYear');
 
   ESCREVA SEUS TESTES ABAIXO:
 */
+
+describe('Requisito 7 - Verifica função ano bissexto', () => {
+  it ('Verifica se retorna um valor booleano', () => {
+    expect(typeof isLeapYear(2020)).toBe('boolean');
+  });
+  it ('Verifica se retorna ano bissexto', () => {
+    expect(isLeapYear(2044)).toBe(true);
+  });
+  it ('Verifica se é inserido um valor em numeral', () => {
+    expect(isLeapYear(expect.any(Number)));
+  });
+});
