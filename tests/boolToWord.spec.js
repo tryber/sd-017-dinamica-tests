cf8a168d1b0c988d257bd049eea80cc5316e3fb9
Carolina Ariadne <c.ariadneadv@gmail.com>
2021-11-14 13:59:37 -0300

    Testes-function-boolToWord

--- tests/boolToWord.spec.js
+++ tests/boolToWord.spec.js
@@ -12,9 +12,24 @@ A função boolToWord recebe um booleano (tamanho variável) e retorna 'Yes' ou 
     - boolToWord('') // Retorno: 'No';
     - boolToWord(true) // Retorno: 'Yes';
     
-    olá
+    
     
     ESCREVA SEUS TESTES ABAIXO:
 */
 
+describe('Implementa casos de testes na função boolToWord `boolToWord`', () => {
+  it('testa se com array vazio retorna yes', () =>  {
+    expect(boolToWord([])).toBe('Yes');
 
+  })
+
+  it('testa se com string vazia retorna No', () => {
+    expect(boolToWord('')).toBe('No');
+  })
+
+  it('testa se com o valor true retorna Yes', () => {
+    expect(boolToWord(true)).toBe('Yes');
+
+  })
+
+})
