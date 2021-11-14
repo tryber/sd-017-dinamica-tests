599ca7f8e440dd1edeed788f8e6840d9b51b0126
kelsonbatista <kelsonbatista@yahoo.co.uk>
2021-11-14 00:54:17 -0300

    Requisito 8 spec

--- tests/myCounter.spec.js
+++ tests/myCounter.spec.js
@@ -1,5 +1,5 @@
 // Importação da função myCounter, não precisa se preocupar com isso por agora.
-const myCounter = require('../src/myCounter');
+const myCounter = require('../src/myCounter.js');
 
 /*
   A função myCounter possui dois loops aninhados que inserem valores dentro de um array. Como podemos perceber, eles vão adicionando valores ao array até sua condição de parada. Corrija o código abaixo para que a função retorne o array correto.
@@ -12,4 +12,11 @@ const myCounter = require('../src/myCounter');
 
   ESCREVA SEUS TESTES ABAIXO:
 */
-
+describe('Requisito 8 - Teste da função myCounter', () => {
+  it ('Verifica se a função retorna um array', () => {
+    expect(typeof myCounter()).toEqual('object');
+  });
+  it ('Verifica se retorna um array com 12 valores', () => {
+    expect(myCounter()).toHaveLength(12);
+  });
+});
