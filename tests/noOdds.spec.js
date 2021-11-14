533ff3486f267c484df264886d2431b928dcaba3
Carolina Ariadne <c.ariadneadv@gmail.com>
2021-11-14 16:01:21 -0300

    teste-function-noOdds

--- tests/noOdds.spec.js
+++ tests/noOdds.spec.js
@@ -1,5 +1,5 @@
 // Importação da função noOdds, não precisa se preocupar com isso por agora.
-const noOdds = require('../src/noOdds.js');
+const noOdds = require("../src/noOdds.js");
 
 /*
 Uma função que, recebe um array (tamanho variável) e verifica se existe elementos que são números pares, retornando um array que não contêm números impares.
@@ -13,3 +13,12 @@ Uma função que, recebe um array (tamanho variável) e verifica se existe eleme
   ESCREVA SEUS TESTES ABAIXO:
 */
 
+describe("Implementa casos de testes na função  `noOdds`", () => {
+  it("testa se  com array que vai de 1 a 7, temos o retorno esperado", () => {
+    expect(noOdds([1, 2, 3, 4, 5, 6])).toEqual([2, 4, 6]);
+  });
+
+  it("testa se  com array 2,7,9,10,16, temos o retorno esperado", () => {
+    expect(noOdds([2, 7, 9, 10, 16])).toEqual([2, 10, 16]);
+  });
+});
